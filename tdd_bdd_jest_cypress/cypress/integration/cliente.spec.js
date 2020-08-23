66361aa48d207ecc4705d7ab7b27e9add604bad8
hernani almeida <her_almeida23@hotmail.com>
2020-08-23 14:12:22 -0300

    testes cypress finalizado

---
+++ tdd_bdd_jest_cypress/cypress/integration/cliente.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000')
+    })
+  
+    // https://on.cypress.io/interacting-with-elements
+  
+    it('ao acessar a home, deve conter uma lista com 10 clientes', () => {
+      // https://on.cypress.io/type
+      cy.get('tbody tr').should('to.have.length', '10')
+      cy.get('tbody tr td').contains('hernani')
+      cy.get('h1').contains('Express')
+      cy.get('body p').contains('Welcome')
+  
+    
+    })
+})
--- tdd_bdd_jest_cypress/models/cliente.js
+++ tdd_bdd_jest_cypress/models/cliente.js
@@ -1,8 +1,8 @@
 module.exports= class Cliente{
-    constructor(){
-        this.id=0
-        this.nome=''
-        this.telefone = ''
+    constructor(_id=0, _nome="", _telefone=""){
+        this.id=_id;
+        this.nome=_nome;
+        this.telefone = _telefone;
     }
     nomeUpercase(){
      return this.nome.toUpperCase()
@@ -14,15 +14,16 @@ module.exports= class Cliente{
    
     static todos(){
         return[
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
+            new Cliente(1, "hernani", "38145832"),
+            new Cliente(2, "hernani2", "38145832"),
+            new Cliente(3, "hernani3", "38145832"),
+            new Cliente(4, "hernani4", "38145832"),
+            new Cliente(5, "hernani5", "38145832"),
+            new Cliente(6, "hernani6", "38145832"),
+            new Cliente(7, "hernani7", "38145832"),
+            new Cliente(8, "hernani8", "38145832"),
+            new Cliente(9, "hernani9", "38145832"),
+            new Cliente(10, "hernani10", "38145832"),
         ]
     }
 }
